0b83c1ed745a6dfaad7e3111a24177e6c1381930
reslaid32 <goteevreslajd@gmail.com>
2025-02-08 13:58:21 +0200

    1.0.1

--- src/ver.c
+++ src/ver.c
@@ -6,6 +6,6 @@ CCOMPRA_PUBLIC_FUNC ccompraver_t ccompraver(void) {
     ccompraver_t compv;
     compv.major = 1;
     compv.minor = 0;
-    compv.patch = 0;
+    compv.patch = 1; /* Update (added compressed bound) */
     return compv;
 }
